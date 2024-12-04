a6eba627e65b47bc57daf1c9efe5edacc189986b
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-12-04 10:11:39 +0100

    <chore>:<try to add firebase, but there is a mistake >

---
+++ src/service/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { addDoc, collection, collectionData, deleteDoc, doc, docData, Firestore, Timestamp, updateDoc } from '@angular/fire/firestore';
+import { User } from '../modules/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private collectionName = "users";
+  constructor(private firestore: Firestore) { }
+  // create new user
+  async createItem(data: User): Promise<void> {
+    const itemsCollection = collection(this.firestore, this.collectionName);
+    await addDoc(itemsCollection, data);
+  }
+}
